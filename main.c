5ffcb83e5c0e6c0f759d95f3546bc6c133dd07d7
Jean-philippe Martel <jemartel@c2r5p5.42quebec.com>
2021-08-16 18:25:34 -0400

    it just work

--- main.c
+++ main.c
@@ -17,25 +17,27 @@ if(keycode == 257)
 
 return(0);
 }
-int display_image(screen *state)
+int display_image(screen *state,int x,int y)
 {
-void *img;
-char *image = "./Kamp.xpm";
-int width;
-int height;
-img =  mlx_xpm_file_to_image(state->mlx,image,&width,&height);
-
-mlx_put_image_to_window(state->mlx,state->win,img,600,600);
-return (0);
-}
+void *img; char *image = "./assets/floor.xpm"; int width; int height; img =  mlx_xpm_file_to_image(state->mlx,image,&width,&height); mlx_put_image_to_window(state->mlx,state->win,img,x,y); return (0); }
 int main(void)
 {
 screen state;
-
+int i = 0;
 
 	state.mlx = mlx_init();
 	state.win = mlx_new_window(state.mlx,1900,1080,"help"); 
-		display_image(&state);
+
+		display_image(&state,650,650);
+		while(i < 1900)
+		{
+	
+			display_image(&state,i,600);
+			display_image(&state,i,655);
+			i += 89;
+		
+		
+		}
 		mlx_key_hook(state.win,close_win,&state);
 	mlx_loop(state.mlx);
 }
