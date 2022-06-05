9729da2379dbcfb9145a139bb17f8cd6befed312
TonyG92x <anthony.guay112@gmail.com>
2022-06-05 15:39:41 -0400

    sike i wasnt done, its pretty good now

--- render/render_texture.c
+++ render/render_texture.c
@@ -6,7 +6,7 @@
 /*   By: anthony <anthony@student.42.fr>            +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 18:00:47 by anthony           #+#    #+#             */
-/*   Updated: 2022/06/05 15:31:21 by anthony          ###   ########.fr       */
+/*   Updated: 2022/06/05 15:38:58 by anthony          ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -25,9 +25,9 @@ void	render_texture(t_game *game, int i, double hauteur)
 	iterator = (HEIGHT / 2) + hauteur;
 	until = (HEIGHT / 2) - hauteur;
 	if (game->ray->last_hit == 'x')
-		asset_width = (((int)game->ray->pos_rayy % game->player->scale) * img->width) / game->player->scale;
+		asset_width = (fmod(game->ray->pos_rayy, (double)game->player->scale) * img->width) / game->player->scale;
 	if (game->ray->last_hit == 'y')
-		asset_width = (((int)game->ray->pos_rayx % game->player->scale) * img->width) / game->player->scale;
+		asset_width = (fmod(game->ray->pos_rayx, game->player->scale) * img->width) / game->player->scale;
 	while (iterator >= until)
 	{
 		asset_height = (x++ * (img->height - 1)) / (hauteur * 2);
