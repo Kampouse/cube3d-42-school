470492c87ac31b11e861fa0df89755a818885bb8
Anthony Guay <aguay@c1r5p2.42quebec.com>
2022-06-10 08:14:37 -0400

    fix rgb

--- render/graphic_operator.c
+++ render/graphic_operator.c
@@ -6,8 +6,7 @@
 /*   By: aguay <aguay@student.42.fr>                +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2021/09/03 18:00:47 by jemartel          #+#    #+#             */
-/*   Updated: 2022/06/08 13:45:54 by jemartel         ###   ########.fr       */
-/*   Updated: 2022/06/06 14:20:52 by aguay            ###   ########.fr       */
+/*   Updated: 2022/06/10 08:12:03 by aguay            ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
@@ -15,7 +14,7 @@
 
 uint32_t	color_to_rgb(uint32_t r, uint32_t g, uint32_t b)
 {
-	return (0xFF | (r << 8) | g << 16 | (b << 24));
+	return (0xFF | (r << 24) | g << 16 | (b << 8));
 }
 
 int	square_shape(t_image *image, int x_pos, int y_pos, uint32_t color)
